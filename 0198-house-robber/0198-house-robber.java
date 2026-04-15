27b79d27d8be487a93bb56f563ed3244d361424e
vtu25507bhanu <vtu25507@veltech.edu.in>
2026-04-15 11:37:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.31%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev2 = 0; // dp[i-2]
+        int prev1 = 0; // dp[i-1]
+
+        for (int num : nums) {
+            int curr = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
