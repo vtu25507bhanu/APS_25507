80bb25b88945e8482e0570269b23fa641033f524
vtu25507bhanu <vtu25507@veltech.edu.in>
2026-04-15 11:20:59 +0530

    Time: 1 ms (78.77%), Space: 42.5 MB (39.93%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int getNext(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
