7d8cc816fd2071456369a1bc383a09f3dcd86a1a
vtu25507bhanu <vtu25507@veltech.edu.in>
2026-04-15 11:07:30 +0530

    Time: 13 ms (92.89%), Space: 49.6 MB (22.04%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] dist = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        // Initialize queue with all 0s and mark 1s as unvisited
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    dist[i][j] = -1;
+                }
+            }
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // BFS
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int r = cell[0], c = cell[1];
+
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return dist;
+    }
+}
